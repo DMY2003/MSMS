e293cf261448f5f40261d0b50e556ef2bc5df824
“MOHAMMED <mohammed.2.chowdhury@kcl.ac.uk>
2022-11-14 23:36:01 +0000

    sign up form and login form

---
+++ lessons/forms.py
@@ -0,0 +1,43 @@
+from django import forms 
+from lessons.models import User
+
+from django.core.validators import RegexValidator
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = {"first_name", "last_name", "email"}
+
+    new_password = forms.CharField(
+        label="Password", 
+        widget=forms.PasswordInput,
+        validators=[
+            RegexValidator(
+                regex=r'^(?=.*[A-Z](?=.*[a-z])(?=.*[0-9])).*$',
+                message="Password must contain an uppercase character, a lowercase character and a number."
+            )
+        ]
+    )
+    confirm_password = forms.CharField(label="Confirm password", widget=forms.PasswordInput)
+
+    def clean(self):
+        super().clean()
+        new_password = self.cleaned_data.get("new_password")
+        confirm_password = self.cleaned_data.get("confirm_password")
+        if new_password != confirm_password:
+            self.add_error("confirm_password", "Confirmation does not match password.")
+
+    def save(self):
+        super().save(commit=False)
+        user = User.objects.create_user(
+            first_name=self.cleaned_data.get("first_name"),
+            last_name=self.cleaned_data.get("last_name"),
+            email=self.cleaned_data.get("email"),
+            password=self.cleaned_data.get("new_password"),
+        )
+        return user
+
+class LogInForm(forms.Form):
+    email = forms.CharField(label="Email")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput())
+
