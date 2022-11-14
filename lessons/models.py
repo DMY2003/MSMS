50aee5fb6fd25923f15103eb321f473f8210f91b
“MOHAMMED <mohammed.2.chowdhury@kcl.ac.uk>
2022-11-14 19:13:39 +0000

    basic models implemented

--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import RegexValidator
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=50, blank=False)
+    last_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(unique=True, blank=False)
+
+class Student(User):
+    balance = models.IntegerField(default=0)
+
+class Administrator(User):
+    pass
+
+class Teacher(User):
+    pass
+
+class Lesson(models.Model):
+    time = models.DateTimeField(blank=False)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, blank=False)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, blank=False)
+
+class Instrument(models.Model):
+    name = models.TextField(blank=False)
