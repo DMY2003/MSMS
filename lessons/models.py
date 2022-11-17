83621404991faa756da1b17039399c1f1edc2b2b
George Korodimos <george.korodmos@kcl.ac.uk>
2022-11-17 22:55:38 +0000

    fixed most tests for user models not working

--- lessons/models.py
+++ lessons/models.py
@@ -1,13 +1,52 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        '''Create and save a user with the given email, and
+        password.
+        '''
+        if not email:
+            raise ValueError('The given email must be set')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(
+                'Superuser must have is_staff=True.'
+            )
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(
+                'Superuser must have is_superuser=True.'
+            )
+
+        return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=50, blank=False)
-    last_name = models.CharField(max_length=50, blank=False)
+    first_name = models.CharField(max_length=50, blank=False, unique=False)
+    last_name = models.CharField(max_length=50, blank=False, unique=False)
     email = models.EmailField(unique=True, blank=False)
-
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
+    REQUIRED_FIELDS = []
 
 class Student(User):
     balance = models.IntegerField(default=0)
--- lessons/tests/user_model_tests.py
+++ lessons/tests/user_model_tests.py
@@ -4,6 +4,16 @@ from django.test import TestCase
 from lessons.models import User
 
 
+def _create_second_user():
+    user = User.objects.create_user(
+        first_name='Jane',
+        last_name='Doe',
+        email='janedoe@example.org',
+        password='Password123',
+    )
+    return user
+
+
 class UserModelTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
@@ -23,7 +33,7 @@ class UserModelTestCase(TestCase):
         self._assert_user_is_invalid()
 
     def test_first_name_need_not_be_unique(self):
-        second_user = self._create_second_user()
+        second_user = _create_second_user()
         self.user.first_name = second_user.first_name
         self._assert_user_is_valid()
 
@@ -41,7 +51,7 @@ class UserModelTestCase(TestCase):
         self._assert_user_is_invalid()
 
     def test_last_name_need_not_be_unique(self):
-        second_user = self._create_second_user()
+        second_user = _create_second_user()
         self.user.last_name = second_user.last_name
         self._assert_user_is_valid()
 
@@ -59,7 +69,7 @@ class UserModelTestCase(TestCase):
         self._assert_user_is_invalid()
 
     def test_email_must_be_unique(self):
-        second_user = self._create_second_user()
+        second_user = _create_second_user()
         self.user.email = second_user.email
         self._assert_user_is_invalid()
 
@@ -86,21 +96,10 @@ class UserModelTestCase(TestCase):
     # helper functions
     def _assert_user_is_valid(self):
         try:
-            self.user.full_clean()
+            self.user.full_clean(exclude=['username'])
         except ValidationError:
             self.fail('Test user should be valid')
 
     def _assert_user_is_invalid(self):
         with self.assertRaises(ValidationError):
-            self.user.full_clean()
-
-    def _create_second_user(self):
-        user = User.objects.create_user(
-            '@janedoe',
-            first_name='Jane',
-            last_name='Doe',
-            email='janedoe@example.org',
-            password='Password123',
-            bio="This is Jane's profile."
-        )
-        return user
+            self.user.full_clean(exclude=['username'])
