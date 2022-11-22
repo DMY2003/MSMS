bcd883bdf5e146aa978edfd05c531bb1ac73f2cf
Samiul I <99480280+Samiul687@users.noreply.github.com>
2022-11-22 05:06:34 +0000

    seeder now works for lessons, invoices and instrument databases. Working on requests database

--- lessons/management/commands/seed.py
+++ lessons/management/commands/seed.py
@@ -1,28 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from faker import Faker
 import faker.providers
-from lessons.models import User, Student, Teacher, Administrator, Invoice, Request
+from lessons.models import User, Student, Teacher, Administrator, Lesson, Invoice, Instrument, Request
 import random
-
-teachers = ["John Doe",
-            "Jane Doe",
-            "Sarah Palmer",
-            "Jessica Swift"]
-
-instruments = ["Guitar",
-               "Ukelele",
-               "Violin",
-               "Recorder",
-               "Piano",
-               "Triangle"]
-
-
-class Provider(faker.providers.BaseProvider):
-    def teachers(self):
-        return self.random_element(teachers)
-
-    def instrument(self):
-        return self.random_element(instruments)
 
 
 def populate_student(fake):
@@ -37,6 +17,7 @@ def populate_student(fake):
         Student.objects.create(first_name=student_fname,
                                last_name=student_lname,
                                email=email,
+                               username=email,
                                balance=balance,
                                password=password,
                                last_login=last_lgn)
@@ -63,38 +44,71 @@ def populate_admin(fake):
         Administrator.objects.create(first_name=admin_fname,
                                      last_name=admin_lname,
                                      email=email,
+                                     username=email,
                                      password=password,
                                      last_login=last_lgn,
                                      is_staff=1,
                                      is_superuser=1)
 
 
-# def populate_requests(fake):
-#     for _ in range(20):
-#         avail = fake.future_datetime()
-#         count = fake.random_int(min=0, max=100)
-#         duration = fake.random_int(min=0, max=240)
-#         t_name = fake.teachers()
-#         instrument = fake.instrument()
-#         student_fname = fake.first_name()
-#         student_lname = fake.last_name()
-#         student = student_fname + " " + student_lname
-#         is_approved = fake.boolean()
-#
-#         Request.objects.create(student_availability=avail,
-#                                lesson_count=count,
-#                                lesson_duration=duration,
-#                                preferred_teacher=t_name,
-#                                instrument=instrument,
-#                                student=student,
-#                                is_approved=is_approved)
+def populate_lessons(fake):
+    teacher_ids = Teacher.objects.values_list('id', flat=True).distinct()
+    student_ids = Student.objects.values_list('id', flat=True).distinct()
+
+    i = 0
+    while i < 50:
+        student_id = Student.objects.get(id=random.choice(student_ids))
+        teacher_id = Teacher.objects.get(id=random.choice(teacher_ids))
+        num_lessons = random.randint(1, 6)
+
+        for _ in range(num_lessons):
+            time = fake.future_datetime()
+            Lesson.objects.create(time=time, teacher=teacher_id, student=student_id)
+            i = i + 1
+
+def populate_invoices():
+    lesson_ids = Lesson.objects.values_list('id', flat=True).distinct()
+
+    for each in lesson_ids:
+        price = random.randint(50, 150)
+        paid = bool(random.getrandbits(1))
+        lesson = Lesson.objects.get(id=each)
+
+        Invoice.objects.create(price=price, paid=paid, lesson=lesson)
+
+def populate_requests(fake):
+    lesson_ids = Lesson.objects.values_list('id', flat=True).distinct()
+    avail = fake.future_datetime()
+    duration = random.choice(range(30, 240, 30))
+    pref_teacher =
+    instriment =
+
+
+# student_availability = models.DateTimeField(blank=False)
+#     lesson_count = models.IntegerField(blank=False)
+#     lesson_duration = models.IntegerField(blank=False)
+#     preferred_teacher = models.TextField()
+#     instrument = models.ForeignKey(Instrument, on_delete=models.CASCADE, blank=False)
+#     student = models.ForeignKey(Student, on_delete=models.CASCADE, blank=False)
+#     is_approved = models.BooleanField(default=False)
+def populate_instruments():
+    instruments = ["Guitar",
+                   "Ukulele",
+                   "Violin",
+                   "Recorder",
+                   "Piano",
+                   "Triangle"]
+
+    for each in instruments:
+        Instrument.objects.create(name=each)
+
+
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write('seeding data...')
         fake = Faker("en_GB")
-        fake.add_provider(Provider)
 
         self.stdout.write('seeding admin...')
         populate_admin(fake)
@@ -102,5 +116,10 @@ class Command(BaseCommand):
         populate_teacher(fake)
         self.stdout.write('seeding student...')
         populate_student(fake)
-
+        self.stdout.write('seeding lessons...')
+        populate_lessons(fake)
+        self.stdout.write('seeding instruments...')
+        populate_instruments()
+        self.stdout.write('seeding invoices...')
+        populate_invoices()
         self.stdout.write('done.')
