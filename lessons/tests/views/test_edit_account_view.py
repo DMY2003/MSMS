7cae8dac3eb8e49ebe161840dc3cb30da8df2bba
Damian Mitrofan <damian.mitrofan@kcl.ac.uk>
2022-12-06 16:11:25 +0000

    More tests for edit account view

--- lessons/tests/views/test_edit_account_view.py
+++ lessons/tests/views/test_edit_account_view.py
@@ -2,8 +2,9 @@
 from django.test import TestCase
 from django.urls import reverse
 from lessons.models import User, Director
+from lessons.tests.helper import LogInTester
 
-class EditAccountViewTestCase(TestCase):
+class EditAccountViewTestCase(TestCase, LogInTester):
     """Tests of the edit account view."""
 
     fixtures = ['lessons/tests/fixtures/default_director.json']
@@ -14,3 +15,21 @@ class EditAccountViewTestCase(TestCase):
 
     def test_admin_manage_url(self):
         self.assertEqual(self.url,'/edit_account/1')
+    
+    def test_admin_manage_view(self):
+        self.login(self.user)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'edit_account.html')
+        self.assertContains(response, 'Edit account')
+        self.assertContains(response, 'First name:')
+        self.assertContains(response, 'Last name:')
+        self.assertContains(response, 'Email:')
+        self.assertContains(response, 'Role:')
+        self.assertContains(response, 'Update')
+        self.assertContains(response, 'Back')
+    
+    def test_admin_manage_view_not_logged_in(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/log_in/?next=/edit_account/1')
