87bc421a902dec714e7f032f9f8d21f089754e8f
Rebi.boy <rebjok911@gmail.com>
2022-12-08 01:03:14 +0000

    fixed tests for the student lessons view

--- lessons/tests/views/test_student_lessons_view.py
+++ lessons/tests/views/test_student_lessons_view.py
@@ -15,8 +15,16 @@ class StudentLessonsViewTestCase(TestCase):
         'lessons/tests/fixtures/other_lessons.json',
         ]
 
+    # fixtures = [
+    #     'lessons/tests/fixtures/default_student.json',
+    #     'lessons/tests/fixtures/other_students.json',
+    #     'lessons/tests/fixtures/default_instrument.json',
+    #     'lessons/tests/fixtures/other_teachers.json',
+    #     'lessons/tests/fixtures/other_lessons.json',
+    #     ]
+
     def setUp(self):
-        self.user = Student.objects.get(id=1)
+        self.user = Student.objects.get(id=2)
         self.url = reverse('student_lessons')
 
     def test_student_lessons_url(self):
@@ -33,39 +41,40 @@ class StudentLessonsViewTestCase(TestCase):
         response = self.client.get(self.url)
         self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
 
-    # def test_student_lessons_displays_lessons_belonging_to_correct_student(self):
-    #     self.client.login(email=self.user.email, password='Password123')
-    #     lesson1 = Lesson.objects.get(id=1234)
-    #     lesson2 = Lesson.objects.get(id=2468)
-    #     lesson3 = Lesson.objects.get(id=1357)
-    #     response = self.client.get(self.url)
-    #     self.assertContains(response, lesson1.id)
-    #     self.assertContains(response, lesson2.id)
-    #     self.assertNotContains(response, lesson3.id)
+    def test_student_lessons_displays_lessons_belonging_to_correct_student(self):
+        self.client.login(email=self.user.email, password='Password123')
+        lesson1 = Lesson.objects.get(id=20)
+        lesson2 = Lesson.objects.get(id=21)
+        lesson3 = Lesson.objects.get(id=24)
+        response = self.client.get(self.url)
+        self.assertContains(response, lesson1.id)
+        self.assertContains(response, lesson2.id)
+        self.assertNotContains(response, lesson3.id)
 
-    # def test_student_lessons_uses_correct_templates(self):
-    #     self.client.login(email=self.user.email, password='Password123')
-    #     response = self.client.get(self.url)
-    #     templates = response.templates
-    #     template_names = [template.name for template in templates]
-    #     self.assertIn("base.html", template_names)
-    #     self.assertIn("base_content.html", template_names)
-    #     self.assertIn("partials/navbar.html", template_names)
-    #     self.assertIn("partials/messages.html", template_names)
-    #     self.assertIn("partials/student_lesson_card.html", template_names)
-    #     self.assertIn('partials/pagination.html', template_names)
+    def test_student_lessons_uses_correct_templates(self):
+        self.client.login(email=self.user.email, password='Password123')
+        response = self.client.get(self.url)
+        templates = response.templates
+        template_names = [template.name for template in templates]
+        self.assertIn("base.html", template_names)
+        self.assertIn("base_content.html", template_names)
+        self.assertIn("partials/navbar.html", template_names)
+        self.assertIn("partials/messages.html", template_names)
+        self.assertIn("partials/student_lesson_card.html", template_names)
+        self.assertIn('partials/pagination.html', template_names)
 
-    # def test_student_lessons_passes_correct_upcoming_lessons_queryset(self):
-    #     self.client.login(email=self.user.email, password='Password123')
-    #     response = self.client.get(self.url)
-    #     upcoming_lessons = response.context["upcoming_lessons"]
-    #     self.assertEqual(len(upcoming_lessons), 2)
-    #     self.assertEqual(upcoming_lessons[0].id, 2468)
-    #     self.assertEqual(upcoming_lessons[1].id, 8642)
+    def test_student_lessons_passes_correct_upcoming_lessons_queryset(self):
+        self.client.login(email=self.user.email, password='Password123')
+        response = self.client.get(self.url)
+        upcoming_lessons = response.context["upcoming_lessons"]
+        self.assertEqual(len(upcoming_lessons), 3)
+        self.assertEqual(upcoming_lessons[0].id, 20)
+        self.assertEqual(upcoming_lessons[1].id, 21)
+        self.assertEqual(upcoming_lessons[1].id, 21)
     
-    # def test_student_request_passes_correct_previous_lessons_queryset(self):
-    #     self.client.login(email=self.user.email, password='Password123')
-    #     response = self.client.get(self.url)
-    #     previous_lessons = response.context["previous_lessons"]
-    #     self.assertEqual(len(previous_lessons), 1)
-    #     self.assertEqual(previous_lessons[0].id, 1234)
+    def test_student_request_passes_correct_previous_lessons_queryset(self):
+        self.client.login(email=self.user.email, password='Password123')
+        response = self.client.get(self.url)
+        previous_lessons = response.context["previous_lessons"]
+        self.assertEqual(len(previous_lessons), 1)
+        self.assertEqual(previous_lessons[0].id, 23)
